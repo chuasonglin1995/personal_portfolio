498099be38289a409acd57c598270c7f934083e2
chuasonglin1995 <chuasonglin1995@gmail.com>
2025-01-28 16:45:01 +0800

    project and contract

---
+++ server.js
@@ -0,0 +1,57 @@
+
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5001, () => console.log("Server Running"));
+console.log(process.env.NODEMAILER_USER);
+console.log(process.env.NODEMAILER_PASSWORD);
+
+// this is not your email password, because you would need 2FA. 
+// Go to google account settings and create an app password.
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASSWORD
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  console.log(req.body);
+  const mail = {
+    from: name,
+    to: "********@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
